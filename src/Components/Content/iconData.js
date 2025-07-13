45c33091e12b3e3fe8be4bfb1125fac46a033332
thanaphon0737 <thanapon081366@gmail.com>
2025-07-13 17:39:29 +0700

    feat: add data for new project

--- src/Components/Content/iconData.js
+++ src/Components/Content/iconData.js
@@ -1,46 +1,75 @@
-import { 
-    FaHtml5, FaCss3, FaJs, FaReact, FaNode, FaBootstrap, FaDocker, 
-    FaGitAlt, FaNpm, FaPython, FaStackOverflow, FaJava, FaVuejs, FaDigitalOcean 
-  } from "react-icons/fa";
-  import { 
-    SiAnaconda, SiExpress, SiOpencv, SiPytorch, SiTensorflow, SiTypescript, 
-    SiVite, SiSocketdotio, SiMariadb, SiHeroku, SiPostgresql, SiTailwindcss 
-  } from "react-icons/si";
-  import { IoLogoVercel } from "react-icons/io5";
-  import { VscTerminalBash } from "react-icons/vsc";
-  import { CgCPlusPlus } from "react-icons/cg";
-  
-  const icons = [
-    { title: "HTML 5", icon: FaHtml5 },
-    { title: "CSS 3", icon: FaCss3 },
-    { title: "JavaScript", icon: FaJs },
-    { title: "React", icon: FaReact },
-    { title: "Node.js", icon: FaNode },
-    { title: "Bootstrap", icon: FaBootstrap },
-    { title: "Docker", icon: FaDocker },
-    { title: "Git", icon: FaGitAlt },
-    { title: "NPM", icon: FaNpm },
-    { title: "Python", icon: FaPython },
-    { title: "Stack Overflow", icon: FaStackOverflow },
-    { title: "Java", icon: FaJava },
-    { title: "Vue.js", icon: FaVuejs },
-    { title: "Digital Ocean", icon: FaDigitalOcean },
-    { title: "Anaconda", icon: SiAnaconda },
-    { title: "Express.js", icon: SiExpress },
-    { title: "OpenCV", icon: SiOpencv },
-    { title: "PyTorch", icon: SiPytorch },
-    { title: "TensorFlow", icon: SiTensorflow },
-    { title: "TypeScript", icon: SiTypescript },
-    { title: "Vite", icon: SiVite },
-    { title: "Socket.io", icon: SiSocketdotio },
-    { title: "MariaDB", icon: SiMariadb },
-    { title: "Heroku", icon: SiHeroku },
-    { title: "PostgreSQL", icon: SiPostgresql },
-    { title: "Tailwind CSS", icon: SiTailwindcss },
-    { title: "Vercel", icon: IoLogoVercel },
-    { title: "Bash", icon: VscTerminalBash },
-    { title: "C++", icon: CgCPlusPlus },
-  ];
-  
-  export default icons;
-  
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaNode,
+  FaBootstrap,
+  FaDocker,
+  FaGitAlt,
+  FaNpm,
+  FaPython,
+  FaStackOverflow,
+  FaJava,
+  FaVuejs,
+  FaDigitalOcean,
+} from "react-icons/fa";
+import {
+  SiAnaconda,
+  SiExpress,
+  SiOpencv,
+  SiPytorch,
+  SiTensorflow,
+  SiTypescript,
+  SiVite,
+  SiSocketdotio,
+  SiMariadb,
+  SiHeroku,
+  SiPostgresql,
+  SiTailwindcss,
+} from "react-icons/si";
+import { IoLogoVercel } from "react-icons/io5";
+import { VscTerminalBash } from "react-icons/vsc";
+import { CgCPlusPlus } from "react-icons/cg";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiFastapi } from "react-icons/si";
+import { SiGooglegemini } from "react-icons/si";
+import { SiLangchain } from "react-icons/si";
+
+const icons = [
+  { title: "HTML 5", icon: FaHtml5 },
+  { title: "CSS 3", icon: FaCss3 },
+  { title: "JavaScript", icon: FaJs },
+  { title: "React", icon: FaReact },
+  { title: "Node.js", icon: FaNode },
+  { title: "Bootstrap", icon: FaBootstrap },
+  { title: "Docker", icon: FaDocker },
+  { title: "Git", icon: FaGitAlt },
+  { title: "NPM", icon: FaNpm },
+  { title: "Python", icon: FaPython },
+  { title: "Stack Overflow", icon: FaStackOverflow },
+  { title: "Java", icon: FaJava },
+  { title: "Vue.js", icon: FaVuejs },
+  { title: "Digital Ocean", icon: FaDigitalOcean },
+  { title: "Anaconda", icon: SiAnaconda },
+  { title: "Express.js", icon: SiExpress },
+  { title: "OpenCV", icon: SiOpencv },
+  { title: "PyTorch", icon: SiPytorch },
+  { title: "TensorFlow", icon: SiTensorflow },
+  { title: "TypeScript", icon: SiTypescript },
+  { title: "Vite", icon: SiVite },
+  { title: "Socket.io", icon: SiSocketdotio },
+  { title: "MariaDB", icon: SiMariadb },
+  { title: "Heroku", icon: SiHeroku },
+  { title: "PostgreSQL", icon: SiPostgresql },
+  { title: "Tailwind CSS", icon: SiTailwindcss },
+  { title: "Vercel", icon: IoLogoVercel },
+  { title: "Bash", icon: VscTerminalBash },
+  { title: "C++", icon: CgCPlusPlus },
+  { title: "Next.js", icon: RiNextjsFill },
+  { title: "FastApi", icon: SiFastapi },
+  { title: "Gemini", icon: SiGooglegemini },
+  { title: "LangChain", icon: SiLangchain}
+];
+
+export default icons;
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,77 +1,119 @@
+import { title } from "framer-motion/client";
+
 const data = [
-    {
-        title: "QR Ordering System For Restaurant",
-        img: "project_vue/thumbnail.png",
-        tools:[
-            {
-                title: "HTML 5"
-            },
-            {
-                title: "CSS 3"
-            },
-            {
-                title: "JavaScript"
-            },
-            {
-                title: "Vue.js"
-            },
-            {
-                title: "Node.js"
-            }
-        ],
-        description: "Developed a real-time web application allowing customers to make restaurant payments using QR codes",
-        url: "https://github.com/thanaphon0737/Qr_system_vue_only"
-    },
-    {
-        title: "PokeDex Clone",
-        img: "project_pokemon/thumbnail.png",
-        tools:[
-            {
-                title: "HTML 5"
-            },
-            {
-                title: "CSS 3"
-            },
-            {
-                title: "JavaScript"
-            },
-            {
-                title: "React"
-            },
-            {
-                title: "Node.js"
-            }
-        ],
-        description: "Created a React-based website providing Pokémon stats and abilities, enhancing my React skills through this interactive project",
-        url: "https://react-poke-api-practice.vercel.app/"
-    },
-    {
-        title: "My first Portfolio with HTML & CSS",
-        img: "project_firstPort/thumbnail.png",
-        tools:[
-            {
-                title: "HTML 5"
-            },
-            {
-                title: "CSS 3"
-            },
-        ],
-        description: "Built a simple website to practice and improve my HTML and CSS skills, focusing on layout and styling",
-        url: "https://thanaphon0737.github.io/makeCV/"
-    },
-    {
-        title: "Machine Learning & Deep Learning Project",
-        img: "project_textTransfer/thumbnail.png",
-        tools:[
-            {
-                title: "PyTorch"
-            },
-            {
-                title: "Python"
-            },
-        ],
-        description: "Worked on a GAN-based project for text transfer tasks. The research has been presented at a conference, exploring the application of GANs in text generation and transformation",
-        url: "https://dl.acm.org/doi/10.1145/3596286.3596288"
-    },
-]
+  {
+    title: "QR Ordering System For Restaurant",
+    img: "project_vue/thumbnail.png",
+    tools: [
+      {
+        title: "HTML 5",
+      },
+      {
+        title: "CSS 3",
+      },
+      {
+        title: "JavaScript",
+      },
+      {
+        title: "Vue.js",
+      },
+      {
+        title: "Node.js",
+      },
+    ],
+    description:
+      "Developed a real-time web application allowing customers to make restaurant payments using QR codes",
+    url: "https://github.com/thanaphon0737/Qr_system_vue_only",
+  },
+  {
+    title: "PokeDex Clone",
+    img: "project_pokemon/thumbnail.png",
+    tools: [
+      {
+        title: "HTML 5",
+      },
+      {
+        title: "CSS 3",
+      },
+      {
+        title: "JavaScript",
+      },
+      {
+        title: "React",
+      },
+      {
+        title: "Node.js",
+      },
+    ],
+    description:
+      "Created a React-based website providing Pokémon stats and abilities, enhancing my React skills through this interactive project",
+    url: "https://react-poke-api-practice.vercel.app/",
+  },
+  {
+    title: "My first Portfolio with HTML & CSS",
+    img: "project_firstPort/thumbnail.png",
+    tools: [
+      {
+        title: "HTML 5",
+      },
+      {
+        title: "CSS 3",
+      },
+    ],
+    description:
+      "Built a simple website to practice and improve my HTML and CSS skills, focusing on layout and styling",
+    url: "https://thanaphon0737.github.io/makeCV/",
+  },
+  {
+    title: "Machine Learning & Deep Learning Project",
+    img: "project_textTransfer/thumbnail.png",
+    tools: [
+      {
+        title: "PyTorch",
+      },
+      {
+        title: "Python",
+      },
+    ],
+    description:
+      "Worked on a GAN-based project for text transfer tasks. The research has been presented at a conference, exploring the application of GANs in text generation and transformation",
+    url: "https://dl.acm.org/doi/10.1145/3596286.3596288",
+  },
+  {
+    title: "Advanced AI-Powered Knowledge Hub",
+    img: "project_knowledgeHub/files.png",
+    tools: [
+      {
+        title: "Python",
+      },
+      {
+        title: "TypeScript",
+      },
+      {
+        title: "Next.js",
+      },
+      {
+        title: "JavaScript",
+      },
+      {
+        title: "Node.js",
+      },
+      {
+        title: "FastApi",
+      },
+      {
+        title: "Gemini",
+      },
+      {
+        title: "Docker",
+      },
+      {
+        title: "LangChain",
+      },
+    ],
+    description:
+      "This project is a personal knowledge management system that uses AI to understand, summarize, and answer questions from various document formats (PDF, images, text) uploaded by the user.",
+    url: "https://github.com/thanaphon0737/knowledge_hub",
+  },
+];
 export default data;
